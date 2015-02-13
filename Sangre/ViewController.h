da58129eac6224c3596fa79ce2655c490a956816
Bharat Mediratta <mediratta@google.com>
2015-02-13 09:08:08 -0800

    Initial Commit

---
+++ Sangre/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sangre
+//
+//  Created by Bharat Mediratta on 2/13/15.
+//  Copyright (c) 2015 Menalto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sangre/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sangre
+//
+//  Created by Bharat Mediratta on 2/13/15.
+//  Copyright (c) 2015 Menalto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
