2f7bec5b3db574087b76b80bf66c2eaf63679326
Bharat Mediratta <mediratta@google.com>
2015-02-14 19:11:53 -0800

    Updates the database - woo\!

--- Sangre/Backend.m
+++ Sangre/Backend.m
@@ -47,9 +47,9 @@
 
 
 - (void) load {
-    NSURL *eventsFeedURL = [NSURL URLWithString:@"https://spreadsheets.google.com/feeds/list/1SCVZzclIEYrSohgpmg5oy4WXWW0P8-3eZnOjRL_dyWc/2/public/full"];
-
     GDataServiceGoogleSpreadsheet *service = [self spreadsheetService];
+    NSURL *eventsFeedURL = [NSURL URLWithString:@"https://spreadsheets.google.com/feeds/list/1SCVZzclIEYrSohgpmg5oy4WXWW0P8-3eZnOjRL_dyWc/2/private/full"];
+
     [service fetchFeedWithURL:eventsFeedURL
                     feedClass:[GDataFeedSpreadsheet class]
                      delegate:self
@@ -106,7 +106,8 @@
 
 - (BackendRow *) rowAt:(NSInteger)index {
     GDataEntrySpreadsheet* row = [[mFeed entries] objectAtIndex:index];
-    return [[BackendRow alloc] initWithTitle:[[row title] stringValue] andContent:[[row content] stringValue]];
+    return [[BackendRow alloc] initWithTitle:[[row title] stringValue]
+                                  andContent:[[row content] stringValue]];
 }
 
 
@@ -118,14 +119,46 @@
 }
 
 -(void) addBgValue:(NSString*)bgValue {
-    GDataEntrySpreadsheet* entry = [[GDataEntrySpreadsheet alloc] init];
-    [entry setTitleWithString:@"1/1/2015 13:00"];
-    [entry setContentWithString:[NSString stringWithFormat:@"bg, %@", bgValue]];
+    NSDate* now = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"MM/dd/YYYY HH:MM:SS";
+    
+    // Convert the date to PST since that's what our spreadsheet expects
+    NSTimeZone *currentTimeZone = [NSTimeZone localTimeZone];
+    NSTimeZone *pstTimeZone = [NSTimeZone timeZoneWithName:@"America/Los_Angeles"];
+    
+    NSInteger currentPstOffset = [currentTimeZone secondsFromGMTForDate:now];
+    NSInteger pstOffset = [pstTimeZone secondsFromGMTForDate:now];
+    NSTimeInterval delta = pstOffset - currentPstOffset;
+    NSDate *pstDate = [[[NSDate alloc] initWithTimeInterval:delta sinceDate:now] autorelease];
+    
+    GDataEntrySpreadsheetList *entry = [GDataEntrySpreadsheetList listEntry];
+    GDataSpreadsheetCustomElement *obj1 =
+        [GDataSpreadsheetCustomElement elementWithName:@"timestamp" stringValue:[formatter stringFromDate:pstDate]];
+    GDataSpreadsheetCustomElement *obj2 =
+        [GDataSpreadsheetCustomElement elementWithName:@"type" stringValue:@"bg"];
+    GDataSpreadsheetCustomElement *obj3 =
+        [GDataSpreadsheetCustomElement elementWithName:@"value" stringValue:bgValue];
+    NSArray* array = [NSArray arrayWithObjects:obj1, obj2, obj3, nil];
+    [entry setCustomElements:array];
 
     [[self spreadsheetService] fetchEntryByInsertingEntry:entry
                                                forFeedURL:[[mFeed postLink] URL]
                                                  delegate:self
-                                        didFinishSelector:@selector(feedTicket:finishedWithFeed:error:)];
+                                        didFinishSelector:@selector(entryTicket:finishedWithFeed:error:)];
+}
+
+- (void)entryTicket:(GDataServiceTicket *)ticket
+  finishedWithFeed:(GDataFeedSpreadsheet *)feed
+             error:(NSError *)error {
+    if (error) {
+        [[UIAlertView alloc] initWithTitle:@"feed error"
+                                   message:[error debugDescription]
+                                  delegate:self
+                         cancelButtonTitle:@"cancel"
+                         otherButtonTitles:@"ok", nil];
+    }
+    [self load];
 }
 
 @end
