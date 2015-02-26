af47acf08592a8fe37b31c4d47be064205cd7d20
Bharat Mediratta <mediratta@google.com>
2015-02-26 14:11:21 -0800

    Load the chart in the background thread.

--- Sangre/ChartsViewController.m
+++ Sangre/ChartsViewController.m
@@ -9,33 +9,30 @@
 #import <Foundation/Foundation.h>
 #import "ChartsViewController.h"
 
-NSString* kChartURL = @"https://docs.google.com/spreadsheets/d/1SCVZzclIEYrSohgpmg5oy4WXWW0P8-3eZnOjRL_dyWc/pubchart?oid=265302356&format=image";
+const NSString* kChartURL = @"https://docs.google.com/spreadsheets/d/1SCVZzclIEYrSohgpmg5oy4WXWW0P8-3eZnOjRL_dyWc/pubchart?oid=265302356&format=image";
 
 @implementation ChartsViewController
 
-- (void)loadImage:(NSString *)chartURL {
-    // download the chart and set it in the imageView
-    chartURL = [chartURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    NSData* chartImageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:chartURL]];
-    UIImage* image = [[UIImage alloc] initWithData:chartImageData];
-    [chartImageData release];
-    
-    // set the image view image to the appropriate size and image for the orientation
-    self.imageView.image = image;
-    self.imageView.frame = CGRectInset(self.view.bounds, 4, 4);
-}
+- (void)loadImage {
+    [self startBeingBusy];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void) {
+        // download the chart and set it in the imageView
+        NSString* chartURL = [kChartURL stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSData* chartImageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:chartURL]];
+        UIImage* image = [[UIImage alloc] initWithData:chartImageData];
+        [chartImageData release];
 
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    [self loadImage:kChartURL];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // set the image view image to the appropriate size and image for the orientation
+            self.imageView.image = image;
+            self.imageView.frame = CGRectInset(self.view.bounds, 4, 4);
+            [self stopBeingBusy];
+        });
+    });
 }
 
 - (void) viewDidAppear:(BOOL)animated {
     [self.navigationController.navigationBar.topItem setTitle:[self.tabBarItem title]];
-}
-
-- (void)dealloc {
-    [_imageView release];
-    [super dealloc];
+    [self loadImage];
 }
 @end
